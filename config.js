a142bffccd4f51cc26977836c3919f784b7538c7
akt2 <atwitchellheyne@gmail.com>
2019-06-15 23:50:49 -0400

    uploading progress so far;

---
+++ config.js
@@ -0,0 +1,2 @@
+
+const api= "pk.eyJ1IjoiYXR3aXRjaGVsbGhleW5lIiwiYSI6ImNqd2lnNzJrdTAyNXEzenBlMzF3dDFtYTgifQ.uKGHjEZpKqIHsZ_dgICX7g";
---
+++ logic.js
@@ -0,0 +1,55 @@
+var url='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson'
+
+d3.json(url,function(d) {
+
+  console.log(d.features);
+
+function onEachFeature(feature,layer) {
+  layer.bindPopup('<h2>'+feature.properties.place+'</h2><hr><p>'
+  +new Date(feature.properties.time)+'</p><hr><p>'
+  +feature.properties.alert+feature.properties.mag+'</p>')
+};
+
+var satellite = L.tileLayer (
+  'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution:
+  'May data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.satellite',
+  accessToken: api
+  });
+
+var outdoors = L.tileLayer (
+  'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 
+  'Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.outdoors',
+  accessToken: api
+});
+
+var bases= {
+  'Satellite Imagery':satellite,
+  'Topographic Imagery':outdoors
+};
+
+var quakes=L.geoJSON(eqd, {
+  onEachFeature: onEachFeature
+});
+
+var overlays= {
+  Earthquakes: quakes
+};
+
+var mymap=L.map('map', {
+  center: [
+    69.40,30.61
+  ],
+  zoom: 5,
+  layers: [satellite,outdoors]
+});
+
+L.control.layers(bases,overlays, {
+  collapsed: false
+}).addTo(mymap);
+});
