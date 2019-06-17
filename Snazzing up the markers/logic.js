9e6be0a31d8ec37c49d7479e9e9fbf2b24b17649
akt2 <atwitchellheyne@gmail.com>
2019-06-17 17:04:21 -0400

    I got colored circles!!! they are static but still YAY

--- Snazzing up the markers/logic.js
+++ Snazzing up the markers/logic.js
@@ -5,22 +5,66 @@ d3.json(url, function(d) {createFeatures(d.features);});
 
 
 function createFeatures(eqd) {
-  function circles(coord,c,s) {return new L.CircleMarker(
-    coord, {color: c, fillColor: c, fillOpacity: 0.75, radius: s},
-      function coord(feature) {L.latlng(feature.geometry.longitude,
-        feature.geometry.latitude)},
-      function coloring(feature) {return c = feature.properties.alert},
-      function sizing(feature) {return s = feature.properties.mag*5000});}
+//   function circles(coord,c,s) {return new L.CircleMarker(
+//     coord, {color: c, fillColor: c, fillOpacity: 0.75, radius: s},
+//       function coord(feature) {L.latlng(feature.geometry.longitude,
+//         feature.geometry.latitude)},
+//       function coloring(feature) {return c = feature.properties.alert},
+//       function sizing(feature) {return s = feature.properties.mag*10000});}
 
       // SURPRISE!! it didn't work BUT progress because my
       // additions no longer ruin everything.
 
-  function onEachFeature(feature, layer) {
-    layer.bindPopup('<h2>'+feature.properties.place+'</h2><hr><p>'
-    +new Date(feature.properties.time).toDateString()+'</p><hr><p>Magnitude: '
-    +feature.properties.mag+'</p>');} 
-  var quakes = L.geoJSON(eqd, {onEachFeature: onEachFeature},{circles:circles});
+  // function onEachFeature(feature, layer) {
+  //   layer.bindPopup('<h2>'+feature.properties.place+'</h2><hr><p>'
+  //   +new Date(feature.properties.time).toDateString()+'</p><hr><p>Magnitude: '
+  //   +feature.properties.mag+'</p>');} 
+
+  // a greater diversity of blunders...
+  // commenting out the onEachFeature block removes the popups and doesn't affect the markers in the slightest
+  // which means my circle debacle is in the wrong place.
+
+  // var quakes = L.geoJSON(eqd, {
+  //   onEachFeature: function onEachFeature(feature, layer) {
+  //     layer.bindPopup('<h2>'+feature.properties.place+'</h2><hr><p>'
+  //     +new Date(feature.properties.time).toDateString()+'</p><hr><p>Magnitude: '
+  //     +feature.properties.mag+'</p>')},
+  //   pointToLayer: function circles(coord,c,s) {return new L.CircleMarker(
+  //     coord, {color: c, fillColor: c, fillOpacity: 0.75, radius: s},
+  //       function coord(feature) {L.latlng(feature.geometry.longitude,
+  //         feature.geometry.latitude)},
+  //       function coloring(feature) {return c = feature.properties.alert},
+  //       function sizing(feature) {return s = feature.properties.mag*10000});}})
+   // function circles(coord,c,s) {return new L.CircleMarker(
+  //         coord, {color: c, fillColor: c, fillOpacity: 0.75, radius: s},
+  //           function coord(feature) {L.latlng(feature.geometry.longitude,
+  //             feature.geometry.latitude)},
+  //           function coloring(feature) {return c = feature.properties.alert},
+  //           function sizing(feature) {return s = feature.properties.mag*10000});}
+
+        // AHA!!! I have found the right place. but everything is broken and very unhappy. Map's still there though! :D
+  var circles = {
+    radius: 20,
+    // radius: function(feature) {return feature.properties.mag*5000},
+    // I MADE A CIRCLE!!!! 10000 is way too big.
+    color: '#e32636',
+    fillColor: '#ff6961',
+    opacity: 0.8,
+    fillOpacity: 0.5
+  };
+
+  // var latlng = function(feature) {L.latlng(feature.geometry.longitude, feature.geometry.latitude);};
+//  redefining latlng isn't necessary apparently.
+
+  var quakes = L.geoJSON(eqd, {
+    pointToLayer: function (feature, latlng) {return new L.CircleMarker(latlng,circles)},
+    onEachFeature: function onEachFeature(feature, layer) {
+      layer.bindPopup('<h2>'+feature.properties.place+'</h2><hr><p>'
+      +new Date(feature.properties.time).toDateString()+'</p><hr><p>Magnitude: '
+      +feature.properties.mag+'</p>')}});
+        
   createMap(quakes);}
+
 
 function createMap(quakes) {
   var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
