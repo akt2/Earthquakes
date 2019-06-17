05240b8b6aa5948cc2b4e13e52839a7a680673fe
akt2 <atwitchellheyne@gmail.com>
2019-06-17 00:18:38 -0400

    making the maps actually appear

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-
-const api= "pk.eyJ1IjoiYXR3aXRjaGVsbGhleW5lIiwiYSI6ImNqd2lnNzJrdTAyNXEzenBlMzF3dDFtYTgifQ.uKGHjEZpKqIHsZ_dgICX7g";
+// API key
+const api = "pk.eyJ1IjoiYXR3aXRjaGVsbGhleW5lIiwiYSI6ImNqd2lnNzJrdTAyNXEzenBlMzF3dDFtYTgifQ.uKGHjEZpKqIHsZ_dgICX7g";
--- logic.js
+++ logic.js
@@ -1,55 +1,40 @@
-var url='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson'
+var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
 
-d3.json(url,function(d) {
+d3.json(url, function(d) {createFeatures(d.features);});
 
-  console.log(d.features);
+function createFeatures(eqd) {
+  function onEachFeature(feature, layer) {
+    layer.bindPopup('<h2>'+feature.properties.place+'</h2><hr><p>'
+    +new Date(feature.properties.time).toDateString()+'</p><hr><p>Magnitude: '
+    +feature.properties.mag+'</p>')}
+  var quakes = L.geoJSON(eqd, {onEachFeature: onEachFeature});
+  createMap(quakes);}
 
-function onEachFeature(feature,layer) {
-  layer.bindPopup('<h2>'+feature.properties.place+'</h2><hr><p>'
-  +new Date(feature.properties.time)+'</p><hr><p>'
-  +feature.properties.alert+feature.properties.mag+'</p>')
-};
-
-var satellite = L.tileLayer (
-  'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution:
-  'May data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox.satellite',
-  accessToken: api
-  });
-
-var outdoors = L.tileLayer (
-  'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 
-  'Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox.outdoors',
-  accessToken: api
-});
-
-var bases= {
-  'Satellite Imagery':satellite,
-  'Topographic Imagery':outdoors
-};
-
-var quakes=L.geoJSON(eqd, {
-  onEachFeature: onEachFeature
-});
-
-var overlays= {
-  Earthquakes: quakes
-};
-
-var mymap=L.map('map', {
-  center: [
-    69.40,30.61
-  ],
-  zoom: 5,
-  layers: [satellite,outdoors]
-});
-
-L.control.layers(bases,overlays, {
-  collapsed: false
-}).addTo(mymap);
-});
+function createMap(quakes) {
+  var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: api});
+  var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.outdoors",
+    accessToken: api});
+  var streetmaps = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: api});
+  var baseMaps = {
+    "Satellite Imagery": satellite,
+    "Topographic Imagery": outdoors,
+    "Infrastructure": streetmaps};
+  var overlayMaps = {
+    Earthquakes: quakes};
+  var myMap = L.map("map", {
+    center: [-21.16, 166.28],
+    zoom: 7.5,
+    layers: [satellite, quakes]});
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false}).addTo(myMap);}
